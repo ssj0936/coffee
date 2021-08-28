12e7cc5bbefdd94eeddb3080d429494de96ebd6f
Timothy <tim77813@gmail.com>
2021-08-28 14:25:12 +0800

    release build

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,12 +1,12 @@
 object Versions{
-    const val gradle = "3.6.1"
+    const val gradle = "4.2.0"
     const val kotlin = "1.4.32"
 
     const val lifecycle = "2.2.0"
     const val retrofit = "2.7.1"
     const val dagger2 = "2.27"
     const val room = "2.2.5"
-    const val gms_location = "17.1.0"
+    const val gms_location = "18.0.0"
     const val rxjava = "3.0.1"
     const val rxjava2_rxandroid = "2.0.1"
     const val timber = "4.7.1"
@@ -14,7 +14,7 @@ object Versions{
     const val conscrypt = "2.5.2"
     const val paging = "2.1.2"
     const val material = "1.2.1"
-    const val play_services_maps = "17.0.0"
+    const val play_services_maps = "17.0.1"
     const val firebase = "28.4.0"
 
     object Androidx{
